879ddbcd1faf50b9b982f12bf3a9e269ce2a25c5
vagrant <vagrant@homestead>
2019-09-06 07:05:03 +0000

    用户显示页面

---
+++ resources/views/shared/_user_info.blade.php
@@ -0,0 +1,5 @@
+<!-- 展示用户的头像和用户名等基本信息 -->
+<a href="{{ route('users.show', $user->id) }}">
+  <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar"/>
+</a>
+<h1>{{ $user->name }}</h1>
