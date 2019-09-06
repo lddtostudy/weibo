c18dd35888bd9874cb7303eadac6de74b9aa7303
lddtostudy <1115953846@qq.com>
2019-09-06 10:01:28 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4026822909,2018141503&fm=26&gp=0.jpg">
+  <a href="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4026822909,2018141503&fm=26&gp=0.jpg" target=_blank>
+    初音未来の新手教程之底部学习
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
